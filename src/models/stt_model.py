32050e2354d0814ceab8b8d1579296c1e0b1f05c
tri <huutri231103@gmail.com>
2025-05-06 15:25:04 +0700

    add test

--- src/models/stt_model.py
+++ src/models/stt_model.py
@@ -385,7 +385,7 @@ class stt_model(nn.Module):
             temperature=kwargs.get("temperature", 1.0),
             attention_mask=attention_mask,
             # bos_token_id=self.tokenizer.bos_token_id,
-            # eos_token_id=self.tokenizer.eos_token_id,
+            eos_token_id=None,
             pad_token_id=self.tokenizer.pad_token_id
         )
 

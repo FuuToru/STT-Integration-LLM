4bea07a3e5aea9e4166602277475d931184733b3
tri <huutri231103@gmail.com>
2025-06-11 17:31:09 +0700

    change prompt

--- src/datasets/speech_dataset.py
+++ src/datasets/speech_dataset.py
@@ -112,7 +112,7 @@ class SpeechDatasetJsonl(torch.utils.data.Dataset):
         if prompt is None:
             # prompt = random.choice(self.prompt_library)
             # prompt = "Transcribe speech to text. "
-            prompt = "Chuyển lời nói thành văn bản. Xuất kết quả chuyển đổi trực tiếp mà không có nội dung dư thừa. Đảm bảo rằng kết quả không bị trùng lặp. "
+            prompt = "Hãy phiên âm lời nói sau thành văn bản tiếng Việt chính xác, không thêm hoặc bỏ sót nội dung."
         prompt = self.prompt_template.format(prompt)
         prompt_ids = self.tokenizer.encode(prompt)
         prompt_length = len(prompt_ids)
